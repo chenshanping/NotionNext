73e7ac06bc134701e8c867bff3fc4d08b70ccbc6
chenshanping <112916616+chenshanping@users.noreply.github.com>
2024-08-15 21:56:14 +0800

    修改浏览器图标

--- blog.config.js
+++ blog.config.js
@@ -25,7 +25,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '云原生小白', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '一名云原生小白', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://hexo.qilincsp.cn/', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://blog.qilincsp.cn/', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
